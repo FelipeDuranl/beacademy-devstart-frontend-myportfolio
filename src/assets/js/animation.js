8b4b698fb393233eac7580c56889d8030b01546d
Felipe Duran Maciel <felipeduram123@gmail.com>
2022-05-15 21:26:06 -0300

    section 3, 4 e 5

---
+++ src/assets/js/animation.js
@@ -0,0 +1,11 @@
+$(window).scroll(function() { 
+    var scroll = $(window).scrollTop();
+    $('#menu').css('background: transparent');
+
+    // if (scroll > 100) {
+    //     $('#menu').css('background: transparent');
+    // } else {
+    //     $('#menu').css('background-color','rgba(23, 23, 23, 0.8)');
+    // }
+});
+
