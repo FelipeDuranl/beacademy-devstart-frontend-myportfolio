11e9efe753830d259b0790c27a7a8616e57adb5f
felipe duran <felipeduram123@gmail.com>
2022-05-16 00:16:53 -0300

    nav responsive

--- src/assets/js/animation.js
+++ src/assets/js/animation.js
@@ -1,4 +1,4 @@
-$(window).scroll(function() { 
+$(window).scroll(function () {
     var scroll = $(window).scrollTop();
     $('#menu').css('background: transparent');
 
@@ -8,4 +8,49 @@ $(window).scroll(function() {
     //     $('#menu').css('background-color','rgba(23, 23, 23, 0.8)');
     // }
 });
+
+/* Scroll reveal - 
+Mostrar o elemento quando der scroll na página */
+
+const scrollReveal = ScrollReveal({
+    origin: "top",
+    distance: "5px",
+    duration: 700,
+    reset: true,
+});
+
+scrollReveal.reveal(
+    `
+  .s-hero .title, 
+  .s-success .tag ,
+  .title-success,
+  .cards,
+  .card,
+  .s-about,
+  .about-me .my-picture,
+  .s-cases,
+  .title-cases,
+  .cases`,
+    { interval: 50 }
+);
+
+const nav = document.querySelector("header .container nav");
+
+const toggle = document.querySelectorAll(".toggle");
+
+for (element of toggle) {
+    element.addEventListener("click", () => {
+        nav.classList.toggle("show");
+    });
+}
+
+const links = document.querySelectorAll(
+    "nav .menu li .menu-item, nav .btn-primary, nav .menu-social li a"
+);
+
+for (const link of links) {
+    link.addEventListener("click", () => {
+        nav.classList.remove("show");
+    });
+}
 
